3c591d18c49182d73f65b9fcb51b1858cdd5c15a
tomerel <tomer@eliyahu.com>
2017-11-14 21:00:11 +0200

    finish

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -6,52 +6,94 @@ def generate_points_n_tags (amount):
     tags = points[:,0] -points[:,1]
     tags = np.sign(tags)
     tags[tags==0] = -1
+    # print(sum (tags))
     return points.T,tags
 
 def show_points (points,tags):
     plt.plot(points[0, :][tags == -1], points[1, :][tags == -1], "ro", zorder = 0)
     plt.plot(points[0, :][tags == 1], points[1, :][tags == 1], "bo", zorder = 0)
 
-def graph(formula, x_range):
+def graph(formula, x_range,color):
     x = np.array(x_range)
-    y = formula(x)  # <- note now we're calling the function 'formula' with x
-    plt.plot(x, y,color = "g")
+    y = formula(x)
+    plt.plot(x, y,color = color)
 
 
 def binary_per(x,y0):
-    w = np.array([1,1])
+    w = np.array([1,1],np.float64)
     x1 = x[0]
     x2 = x[1]
     signs = np.zeros(len(y0))
     iter_counter = 0
     while (not np.array_equal(signs, y0)):
+        # print("signs =", signs)
+        # print ("yo = ", y0)
         for i in range (len (x1)):
             signs[i] = np.sign (w[0]*x1[i] + w[1]*x2[i])
             if (signs[i] != y0[i] or (iter_counter == 0 and i == 0)):
                 w[0] = w[0] + y0[i]*x1[i]
-                w[1] = w[1] + y0[i] * x2[i]
+                w[1] = w[1] + y0[i]*x2[i]
         iter_counter += 1
-    print ("iter_counter", iter_counter)
-    return  (w)
-    # print (x1[i],x2[i], y0[i])
+    return  (w/np.linalg.norm(w))
 
+def stimulate(to_repeat, n_points):
+    w_s = []
+    ideal = np.array([1,-1])
+    plt.quiver(ideal[0], ideal[1], color="black", units="xy", zorder=1)
+
+    for i in range(to_repeat):
+        points, tags = generate_points_n_tags(n_points)
+        # if (n_points <1000):
+        #
+        #     show_points(points,tags)
+        w = binary_per(points, tags)
+        w_s.append(w.copy())
+        # error = 1/to_repeat
+        calc_line = lambda x: (-w[0] * x) / w[1]
+        color = np.random.rand(3,1)
+        graph(calc_line, range(int(points[0].min()), int(points[0].max()+1)),color)
+        plt.quiver(w[0], w[1], color=color, units="xy", zorder=1)
+    w_s = np.asarray(w_s)
+    err = 0
+    for i in range(len(w_s)):
+        x = np.dot(w_s[i],ideal)
+        y = np.linalg.norm(ideal )* np.linalg.norm(w_s[i])
+        err+= np.fabs(np.arccos(x/y))
+    err = err /to_repeat
+    return  err
+
+#A
 points, tags = generate_points_n_tags(1000)
-my_points = np.array([2,1,-1,-1,
-                      3,3,-1,2]).reshape(2,4)
-my_y = np.array([1,1,-1,-1])
-
 show_points(points,tags)
+
+#B
 w = binary_per(points,tags)
-print(w)
-formula = lambda x: (-w[0]*x)/w[1]
-graph(formula, range(int(points[0].min()),int(points[1].max())))
-plt.plot ()
-plt.quiver(w[0],w[1],color='black', units = "y", zorder = 1)
 
-axes = plt.axis()
+
+#C
+calc_line = lambda x: (-w[0]*x)/w[1]
+color = np.random.rand(3,1)
+graph(calc_line, range(int(points[0].min()),int(points[1].max())),color)
+plt.quiver(w[0],w[1],color=color, units = "y", zorder = 1, width = 0.2)
 plt.show()
-print(my_points)
-# print(points)
-# print(tags)
 
-# print (points)
+#D
+plt.figure(0)
+error = stimulate(10, 1000)
+plt.figure(1)
+error = stimulate(10,25)
+
+plt.show()
+
+#E
+errors = []
+p = [25,35,55,100,150,200,500]
+for points_n in p:
+    errors.append(stimulate(100,points_n))
+
+plt.clf() #clear plot
+
+plt.plot (p,errors,"--o")
+plt.xlabel("p")
+plt.ylabel("error")
+plt.show()
